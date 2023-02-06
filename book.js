9c12125f48285fd42085d21e2bc67cd743bbe348
Manatsawin Hanmongkolchai <git@whs.in.th>
2023-02-06 15:36:20 +0700

    Actually add the files

---
+++ book.js
@@ -0,0 +1,21 @@
+import path from "path";
+import fs from "fs/promises";
+import express from "express";
+const BOOK_DIR = path.resolve("books");
+export default function register(app) {
+  const subrouter = express.Router();
+  app.use("/bookService", subrouter);
+  subrouter.get("/list", async (req, res) => {
+    const files = await fs.readdir(BOOK_DIR);
+    res.json({ books: files });
+    res.end();
+  });
+  subrouter.get("/get/:name", async (req, res) => {
+    const bookPath = path.resolve(BOOK_DIR, req.params.name);
+    if (!bookPath.startsWith(BOOK_DIR)) {
+      res.statusCode = 400;
+      res.end();
+    }
+    res.sendFile(bookPath);
+  });
+}
